406d82f844e255c9ef4365b60b4b4990b39b9d7b
GreatfruitGames <145738804+GreatfruitGames@users.noreply.github.com>
2026-08-08 23:01:53 -0400

    All enemies
    All 5 enemy types have been finished

--- Scripts/enemy_base.gd
+++ Scripts/enemy_base.gd
@@ -3,13 +3,21 @@ extends RigidBody2D
 @export_enum("Chaser", "Bouncer", "Chouncer", "Flyer", "Chlyer") var enemy_type : String
 @export var bounceTimer : Timer
 @export var level : Node2D
+@export var detector : Area2D
+
+var target
 
 # The layer the enemy is on
 @export var layer : int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if enemy_type == "Flyer" or enemy_type == "Chlyer":
+		gravity_scale = 0
+		physics_material_override.bounce = 0
+	if enemy_type == "Chaser":
+		physics_material_override.bounce = 0
+		mass = 20
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -33,19 +41,27 @@ func move():
 		chly()
 
 func chase():
-	pass
+	if target != null:
+		apply_central_impulse((target.global_position - self.global_position))
 
 func bounce():
-	#if Vector2(roundi(linear_velocity.x), roundi(linear_velocity.y)) == Vector2(0,0):
 	if bounceTimer.is_stopped():
 		apply_central_impulse(Vector2(randf_range(-1000,1000),randf_range(-1000, -500)))
 		bounceTimer.start()
 
 func chounce():
-	pass
+	if bounceTimer.is_stopped() and target != null:
+		apply_central_impulse((target.global_position - self.global_position) * 10)
+		bounceTimer.start()
 
 func fly():
-	pass
+	if bounceTimer.is_stopped():
+		apply_central_impulse(Vector2(randf_range(-100,100),randf_range(-100, 100)))
+		bounceTimer.start()
 
 func chly():
-	pass
+	if target != null:
+		apply_central_impulse((target.global_position - self.global_position) / 20)
+
+func _on_search_body_entered(body: Node2D) -> void:
+	target = body
